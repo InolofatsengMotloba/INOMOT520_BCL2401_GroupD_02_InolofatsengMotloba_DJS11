38d13d32ada369989fbf1510bec20e1b496815b3
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-18 13:44:40 +0200

    Installed react-router-dom dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import './App.css'
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
-  
-
   return (
-    <>
-    
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
+    // <h1>hello</h1>
+  );
 }
 
-export default App
+export default App;
