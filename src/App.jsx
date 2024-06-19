c1ba132cee8fe04e853e57dc3654dd2aae733e6a
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-20 00:12:32 +0200

    Installed styled-components dependencies & Added theme provider to toggle between light and dark modes for the app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./utils/Themes";
+
+const Container = styled.div`
+  background: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100vh;
+`;
+
 function App() {
-  return <h1>hello</h1>;
+  const [darkMode, setDarkMode] = useState(true);
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>iListen</Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/utils/Themes.jsx
+++ src/utils/Themes.jsx
@@ -1,19 +1,19 @@
-export const lightTheme = {
-  bg: "#FFFFFF",
-  bgLight: "#f0f0f0",
-  primary: "#3a5745",
-  text_primary: "#111111",
-  text_secondary: "#48494a",
-  card: "#FFFFFF",
+export const darkTheme = {
+  bg: "#15171E",
+  bgLight: "#1C1E27",
+  primary: "#be1adb",
+  text_primary: "#F2F3F4",
+  text_secondary: "#b1b2b3",
+  card: "#121212",
   button: "#5c5b5b",
 };
 
-export const darkTheme = {
-  bg: "#3a5745",
-  bgLight: "#516756",
-  primary: "#FFFFFF",
-  text_primary: "#F2F3F4",
-  text_secondary: "#b1b2b3",
-  card: "#3a5745",
+export const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#f0f0f0",
+  primary: "#be1adb",
+  text_primary: "#111111",
+  text_secondary: "#48494a",
+  card: "#FFFFFF",
   button: "#5c5b5b",
 };
