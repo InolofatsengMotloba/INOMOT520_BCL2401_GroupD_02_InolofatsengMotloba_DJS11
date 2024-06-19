cb76fbd10fe1cb111142c292bb7e12341f892958
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-19 14:19:20 +0200

    Add light and dark theme configurations for the app

---
+++ src/utils/Themes.jsx
@@ -0,0 +1,19 @@
+export const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#f0f0f0",
+  primary: "#3a5745",
+  text_primary: "#111111",
+  text_secondary: "#48494a",
+  card: "#FFFFFF",
+  button: "#5c5b5b",
+};
+
+export const darkTheme = {
+  bg: "#3a5745",
+  bgLight: "#516756",
+  primary: "#FFFFFF",
+  text_primary: "#F2F3F4",
+  text_secondary: "#b1b2b3",
+  card: "#3a5745",
+  button: "#5c5b5b",
+};
