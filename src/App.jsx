287921d3f6b6c1267027c7a06fc1883dc39bb4e5
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-19 10:28:02 +0200

    Delete unused file & unsused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-
-      </Routes>
-    </BrowserRouter>
-    // <h1>hello</h1>
-  );
+  return <h1>hello</h1>;
 }
 
 export default App;
