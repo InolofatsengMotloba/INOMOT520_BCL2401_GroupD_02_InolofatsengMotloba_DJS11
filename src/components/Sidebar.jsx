72a6418aadbe23bc74ef6cd3df7bf11581ab44be
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-20 01:33:42 +0200

    Style MenuContainer & Logo element with theme properties

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,41 @@ import React from "react";
 import styled from "styled-components";
 import { HomeRounded, CloseRounded } from "@mui/icons-material";
 
-const MenuContainer = styled.div``;
-const Logo = styled.div``;
+const MenuContainer = styled.div`
+  flex: 0.5;
+  flex-direction: column;
+  height: 100vh;
+  display: flex;
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  //   @media (max-width: 1100px) {
+  //     position: fixed;
+  //     z-index: 1000;
+  //     width: 100%;
+  //     max-width: 250px;
+  //     left: ${({ setMenuOpen }) => (setMenuOpen ? "0" : "-100%")};
+  //     transition: 0.3s ease-in-out;
+  //   }
+`;
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Logo = styled.div`
+  color: ${({ theme }) => theme.primary};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 6px;
+  font-weight: bold;
+  font-size: 20px;
+  margin: 16px 0px;
+`;
+
 const Close = styled.div``;
 const Elements = styled.div``;
 const NavText = styled.div``;
@@ -11,10 +44,13 @@ const NavText = styled.div``;
 const Sidebar = () => {
   return (
     <MenuContainer>
-      <Logo></Logo>
-      <Close>
-        <CloseRounded />
-      </Close>
+      <Flex>
+        <Logo>iListen</Logo>
+        <Close>
+          <CloseRounded />
+        </Close>
+      </Flex>
+
       <Elements>
         <HomeRounded />
         <NavText>Dashboard</NavText>
