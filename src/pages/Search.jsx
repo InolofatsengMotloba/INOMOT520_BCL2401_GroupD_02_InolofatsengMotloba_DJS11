0bee23142c9410ce100237b32144db7bc1fd32a9
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-21 02:52:47 +0200

    Implement hoos to handle input changes

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import SearchOutlinedIcon  from "@mui/icons-material/SearchOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
 const Searchmain = styled.div`
   padding: 20px 30px;
@@ -28,16 +28,35 @@ const SearchBar = styled.div`
   color: ${({ theme }) => theme.text_secondary};
 `;
 
-
 export const Search = () => {
+  const [searched, setSearched] = useState("");
+
+  const handleChange = async (e) => {
+    setSearched(e.target.value);
+  };
+
   return (
     <Searchmain>
       <div className="searchBarContainer">
         <SearchBar>
           <SearchOutlinedIcon sx={{ color: "inherit" }} />
-          <input type="text" placeholder="Search Artist/Podcasts" className="searchInput" />
+          <input
+            type="text"
+            placeholder="Search Artist/Podcasts"
+            className="searchInput"
+            value={searched}
+            onChange={(e) => handleChange(e)}
+          />
         </SearchBar>
       </div>
+      {searched === "" ? (
+        <Categories>
+          <Heading>Browse All</Heading>
+          <BrowseAll></BrowseAll>
+        </Categories>
+      ) : (
+        <>Hi</>
+      )}
     </Searchmain>
   );
 };
