039d19494b829e905e7ae9ec986457d1643d7060
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-21 02:00:41 +0200

    Installed dependencies for carousel

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { PodcastCard } from "../components/PodcastCard";
+import Slider from "react-slick";
 
 const DashboardMain = styled.div`
   padding: 20px 30px;
